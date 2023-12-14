bc3c84c639c25728d9d53e83b207df0201136426
Ivo <ivobankov758@gmail.com>
2023-12-14 23:09:38 +0200

    add company

--- project source/src/main/java/org/example/entity/Company.java
+++ project source/src/main/java/org/example/entity/Company.java
@@ -13,4 +13,27 @@ public class Company {
     @Column(name = "name")
     private String name;
 
+    public Company() {
+    }
+
+    public Company(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
